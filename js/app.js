2ef192cb2fbb0db8ad7d136a919a08c9cf79e856
duridev <duridev@outlook.com>
2023-11-24 10:08:57 -0300

    Creó mensaje de error en validacion de campos

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const inputConfirmarEmail = document.querySelector('#confirmarEmail')
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
+    const formulario = document.querySelector('#formulario')
 
     inputEmail.addEventListener('blur', validar);
     inputConfirmarEmail.addEventListener('blur', validar);
@@ -12,9 +13,22 @@ document.addEventListener('DOMContentLoaded', function() {
     inputMensaje.addEventListener('blur', validar);
 
   function validar(evt) {
-    console.log(evt.target.value);
+    if(evt.target.value.trim() === "") {
+      mostrarAlerta();
+    } else {
+      console.log('Hay algo');
+    }
 
   }
 
+  function mostrarAlerta() {
+    // Generar alerta en HTML
+    const error = document.createElement('P');
+    error.textContent = 'Todos los campos son obligatorios';
+    error.classList.add('bg-red-600', 'text-white', 'p-2', 'font-bold', 'text-center');
+
+    // Inyectar el error al formulario
+    formulario.appendChild(error);
+  }
 
 });
